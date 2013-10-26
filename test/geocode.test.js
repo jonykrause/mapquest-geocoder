fe9a108b8ea46e3263ed666bb2f4763e9ba3a944
jonykrause <buero@jonathan-krause.de>
2013-10-26 12:56:19 +0200

    update test

--- test/geocode.test.js
+++ test/geocode.test.js
@@ -3,86 +3,87 @@ var Geocoder = require('../index')
 
 
 var testData = {
-  validAddressList: ['Afghanistan, Kabul', 'Albania, Tirana', 'Unter den Linden 17, Berlin, Germany'],
-  invalidAddressList: ['Afghanistan, Kabul', 'foo, bar, baz', 'Unter den Linden 17, Berlin, Germany'],
+  validAddressList: ['Kabul, Afghanistan' , 'Albania, Tirana', 'Unter den Linden 17, Berlin, Germany'],
+  invalidAddressList: ['Kabul, Afghanistan', 'foo, bar, baz', 'Unter den Linden 17, Berlin, Germany'],
   validAddress: 'Unter den Linden 17, Berlin, Germany',
   invalidAddress: 'foo, bar, baz',
   latLong: '52.516815, 13.390421'
 }
 
+describe('Geocoder', function() {
 
-describe('#geocode("valid address")', function() {
-  it('should emit with received data', function(done) {
-    var geocoder = new Geocoder;      
-    geocoder.geocode(testData.validAddress);
-    
-    geocoder.on('geocoding:finished', function(geocodedLocations) {         
-      should.exist(geocodedLocations.received);
-      should.not.exist(geocodedLocations.rejected);
-      geocodedLocations.received.should.not.be.empty;
-      done();
+  beforeEach(function() {
+    geocoder = new Geocoder;
+  });
+
+  describe('#geocode("valid address")', function() {
+    it('should emit with received data', function(done) {
+      geocoder.geocode(testData.validAddress);
+
+      geocoder.on('geocoding:finished', function(geocodedLocations) {
+        should.exist(geocodedLocations.received);
+        should.not.exist(geocodedLocations.rejected);
+        geocodedLocations.received.should.not.be.empty;
+        done();
+      })
     })
-  })
-});  
+  });
 
 
-describe('#geocode("invalid address")', function() {
-  it('should emit with rejected data', function(done) {
-    var geocoder = new Geocoder;
-    geocoder.geocode(testData.invalidAddress);
+  describe('#geocode("invalid address")', function() {
+    it('should emit with rejected data', function(done) {
+      geocoder.geocode(testData.invalidAddress);
 
-    geocoder.on('geocoding:finished', function(geocodedLocations) {         
-      should.exist(geocodedLocations.rejected);
-      should.not.exist(geocodedLocations.received);
-      geocodedLocations.rejected.should.not.be.empty;
-      done();
+      geocoder.on('geocoding:finished', function(geocodedLocations) {
+        should.exist(geocodedLocations.rejected);
+        should.not.exist(geocodedLocations.received);
+        geocodedLocations.rejected.should.not.be.empty;
+        done();
+      })
     })
-  })
+  });
+
+
+  describe('#geocode(lat, long")', function() {
+    it('should emit with received data', function(done) {
+      geocoder.geocode(testData.latLong, { reverse: true });
+
+      geocoder.on('geocoding:finished', function(geocodedLocations) {
+        should.exist(geocodedLocations.received);
+        should.not.exist(geocodedLocations.rejected);
+        geocodedLocations.received.should.not.be.empty;
+        done();
+      })
+    })
+  });
+
+
+  describe('#geocode([valid list])', function() {
+    it('should emit with received data', function(done) {
+      geocoder.geocode(testData.validAddressList);
+
+      geocoder.on('geocoding:finished', function(geocodedLocations) {
+        should.exist(geocodedLocations.received);
+        should.not.exist(geocodedLocations.rejected);
+        geocodedLocations.received.should.have.length(3);
+        done();
+      })
+
+    })
+  });
+
+
+  describe('#geocode([invalid list])', function() {
+    it('should emit with received and rejected data not being empty', function(done) {
+      geocoder.geocode(testData.invalidAddressList);
+
+      geocoder.on('geocoding:finished', function(geocodedLocations) {
+        should.exist(geocodedLocations.received);
+        should.exist(geocodedLocations.rejected);
+        geocodedLocations.rejected.should.have.length(1);
+        geocodedLocations.received.should.have.length(2);
+        done();
+      })
+    })
+  });
 });
-
-
-describe('#geocode(lat, long")', function() {
-  it('should emit with received data', function(done) {
-    var geocoder = new Geocoder;
-    geocoder.geocode(testData.latLong, { reverse: true });
-
-    geocoder.on('geocoding:finished', function(geocodedLocations) {         
-      should.exist(geocodedLocations.received);
-      should.not.exist(geocodedLocations.rejected);
-      geocodedLocations.received.should.not.be.empty;
-      done();
-    })
-  })
-});
-
-
-describe('#geocode([valid list])', function() {  
-  it('should emit with received data', function(done) {
-    var geocoder = new Geocoder;      
-    geocoder.geocode(testData.validAddressList);
-
-    geocoder.on('geocoding:finished', function(geocodedLocations) {         
-      should.exist(geocodedLocations.received);
-      should.not.exist(geocodedLocations.rejected);
-      geocodedLocations.received.should.have.length(3);
-      done();
-    })
-
-  })
-});  
-
-
-describe('#geocode([invalid list])', function() {  
-  it('should emit with received and rejected data not being empty', function(done) {    
-    var geocoder = new Geocoder;      
-    geocoder.geocode(testData.invalidAddressList);
-
-    geocoder.on('geocoding:finished', function(geocodedLocations) {         
-      should.exist(geocodedLocations.received);
-      should.exist(geocodedLocations.rejected);
-      geocodedLocations.rejected.should.have.length(1);
-      geocodedLocations.received.should.have.length(2);
-      done();
-    })
-  })
-}); 
